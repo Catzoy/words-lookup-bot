946e329f931af6975bdee583b6403dc54ebb3f09
Dmytro Demchenko <demchenko20021@gmail.com>
2025-08-18 07:23:37 +0000

    Simple bot setup

--- src/main.rs
+++ src/main.rs
@@ -1,131 +1,55 @@
-mod weather;
-
 use anyhow::Context as _;
-use serenity::all::{GuildId, Interaction};
-use serenity::async_trait;
-use serenity::builder::{
-    CreateCommand, CreateCommandOption, CreateInteractionResponse, CreateInteractionResponseMessage,
-};
-use serenity::model::gateway::Ready;
-use serenity::prelude::*;
-use shuttle_runtime::SecretStore;
-use tracing::info;
+use shuttle_runtime::{Error, SecretStore};
+use std::net::SocketAddr;
+use teloxide::prelude::Requester;
+use teloxide::types::Message;
+use teloxide::{repl, Bot};
 
-struct Bot {
-    weather_api_key: String,
-    client: reqwest::Client,
-    discord_guild_id: GuildId,
+pub struct TelegramService {
+    token: String,
 }
 
-#[async_trait]
-impl EventHandler for Bot {
-    async fn ready(&self, ctx: Context, ready: Ready) {
-        info!("{} is connected!", ready.user.name);
-
-        let commands = vec![
-            CreateCommand::new("hello").description("Say hello"),
-            CreateCommand::new("weather")
-                .description("Display the weather")
-                .add_option(
-                    CreateCommandOption::new(
-                        serenity::all::CommandOptionType::String,
-                        "place",
-                        "City to lookup forecast",
-                    )
-                    .required(true),
-                ),
-        ];
-
-        let commands = &self
-            .discord_guild_id
-            .set_commands(&ctx.http, commands)
-            .await
-            .unwrap();
-
-        info!("Registered commands: {:#?}", commands);
+impl TelegramService {
+    pub fn new(token: String) -> Self {
+        TelegramService { token }
     }
+}
 
-    async fn interaction_create(&self, ctx: Context, interaction: Interaction) {
-        if let Interaction::Command(command) = interaction {
-            let response_content = match command.data.name.as_str() {
-                "hello" => "hello".to_owned(),
-                "weather" => {
-                    let argument = command
-                        .data
-                        .options
-                        .iter()
-                        .find(|opt| opt.name == "place")
-                        .cloned();
+#[shuttle_runtime::async_trait]
+impl shuttle_runtime::Service for TelegramService {
+    async fn bind(self, _: SocketAddr) -> Result<(), Error> {
+        let client = reqwest::Client::builder()
+            .build()
+            .expect("Could not build request client");
+        let bot = Bot::with_client(self.token, client);
 
-                    let value = argument.unwrap().value;
-                    let place = value.as_str().unwrap();
-
-                    let result =
-                        weather::get_forecast(place, &self.weather_api_key, &self.client).await;
-
-                    match result {
-                        Ok((location, forecast)) => {
-                            format!("Forecast: {} in {}", forecast.headline.overview, location)
-                        }
-                        Err(err) => {
-                            format!("Err: {err}")
-                        }
-                    }
+        repl(bot, |bot: Bot, message: Message| async move {
+            log::info!("Handling a new message!");
+            log::debug!("Message: {:#?}", message);
+            let result = bot.send_message(message.chat.id, "Hi there").await;
+            match result {
+                Ok(_) => {
+                    log::info!("Sent successfully!");
                 }
-                command => unreachable!("Unknown command: {}", command),
-            };
-
-            let data = CreateInteractionResponseMessage::new().content(response_content);
-            let builder = CreateInteractionResponse::Message(data);
-
-            if let Err(why) = command.create_response(&ctx.http, builder).await {
-                println!("Cannot respond to slash command: {why}");
+                Err(e) => {
+                    log::error!("Error while sending message: {}", e);
+                }
             }
-        }
+            Ok(())
+        }).await;
+
+        Ok(())
     }
 }
 
 #[shuttle_runtime::main]
-async fn serenity(
+async fn telegram(
     #[shuttle_runtime::Secrets] secret_store: SecretStore,
-) -> shuttle_serenity::ShuttleSerenity {
-    // Get the discord token set in `Secrets.toml`
-    let discord_token = secret_store
-        .get("DISCORD_TOKEN")
-        .context("'DISCORD_TOKEN' was not found")?;
+) -> Result<TelegramService, Error> {
+    let token = secret_store
+        .get("TELOXIDE_TOKEN")
+        .context("TELOXIDE_TOKEN not found")?;
+    let service = TelegramService::new(token);
 
-    let weather_api_key = secret_store
-        .get("WEATHER_API_KEY")
-        .context("'WEATHER_API_KEY' was not found")?;
-
-    let discord_guild_id = secret_store
-        .get("DISCORD_GUILD_ID")
-        .context("'DISCORD_GUILD_ID' was not found")?;
-
-    let client = get_client(
-        &discord_token,
-        &weather_api_key,
-        discord_guild_id.parse().unwrap(),
-    )
-    .await;
-    Ok(client.into())
-}
-
-pub async fn get_client(
-    discord_token: &str,
-    weather_api_key: &str,
-    discord_guild_id: u64,
-) -> Client {
-    // Set gateway intents, which decides what events the bot will be notified about.
-    // Here we don't need any intents so empty
-    let intents = GatewayIntents::empty();
-
-    Client::builder(discord_token, intents)
-        .event_handler(Bot {
-            weather_api_key: weather_api_key.to_owned(),
-            client: reqwest::Client::new(),
-            discord_guild_id: GuildId::new(discord_guild_id),
-        })
-        .await
-        .expect("Err creating client")
+    Ok(service)
 }
