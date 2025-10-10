7ab83ecda5face7fdcbe594517e9757104f9ca69
catzoy <demchenko20021@gmail.com>
2025-10-10 19:30:21 +0300

    Version 0.9.1
    Refactor suggestions handler into a simple list processing pipeline

--- src/inlines/suggestions.rs
+++ src/inlines/suggestions.rs
@@ -1,6 +1,7 @@
 use crate::bloc::common::HandlerOwner;
 use crate::bloc::word_lookup::WordLookupFormatter;
 use crate::format::ToEscaped;
+use crate::wordle::WordleDayAnswer;
 use crate::{
     commands::{FullMessageFormatter, MessageCommands},
     format::LookupFormatter,
@@ -15,55 +16,53 @@ use teloxide::types::{
 use teloxide::utils::command::BotCommands;
 use teloxide::{prelude::Requester, types::InlineQuery, Bot};
 
-pub struct SuggestionsOwner;
-impl SuggestionsOwner {
-    async fn ensure_wordle_answer(mut cache: WordleCache) -> anyhow::Result<()> {
-        let fresh_result = cache.require_fresh_answer().await;
-        if let Err(err) = fresh_result {
-            log::error!("Failed to get today's wordle, err: {}", err);
-        }
-        Ok(())
+trait SuggestionOwner {
+    fn produce(&self) -> Option<InlineQueryResult>;
+}
+
+struct HelpSuggestion;
+impl SuggestionOwner for HelpSuggestion {
+    fn produce(&self) -> Option<InlineQueryResult> {
+        let text = "Continue writing to look up a word or a phrase";
+        let msg = MessageCommands::descriptions().to_string();
+        let msg = InputMessageContentText::new(msg.to_escaped());
+        let msg = InputMessageContent::Text(msg);
+        let msg = InlineQueryResultArticle::new("help", text, msg);
+        Some(InlineQueryResult::Article(msg))
     }
+}
 
-    async fn suggestions_handler(
-        bot: Bot,
-        query: InlineQuery,
-        mut cache: WordleCache,
-    ) -> anyhow::Result<()> {
-        let mut answers = Vec::<InlineQueryResult>::new();
-        let help = {
-            let text = "Continue writing to look up a word or a phrase";
-            let msg = MessageCommands::descriptions().to_string();
-            let msg = InputMessageContentText::new(msg.to_escaped());
-            let msg = InputMessageContent::Text(msg);
-            let msg = InlineQueryResultArticle::new("help", text, msg);
-            InlineQueryResult::Article(msg)
-        };
-        answers.push(help);
+struct UrbanSuggestion;
+impl SuggestionOwner for UrbanSuggestion {
+    fn produce(&self) -> Option<InlineQueryResult> {
+        let text = "Or write \"u.PHRASE\" to look up in UrbanDictionary";
+        let msg = InputMessageContentText::new(
+            "Write @WordsLookupBot \"u.PHRASE\" to look up in UrbanDictionary",
+        );
+        let msg = InputMessageContent::Text(msg);
+        let msg = InlineQueryResultArticle::new("urban", text, msg);
+        Some(InlineQueryResult::Article(msg))
+    }
+}
 
-        let urban = {
-            let text = "Or write \"u.PHRASE\" to look up in UrbanDictionary";
-            let msg = InputMessageContentText::new(
-                "Write @WordsLookupBot \"u.PHRASE\" to look up in UrbanDictionary",
-            );
-            let msg = InputMessageContent::Text(msg);
-            let msg = InlineQueryResultArticle::new("urban", text, msg);
-            InlineQueryResult::Article(msg)
-        };
-        answers.push(urban);
+struct ThesaurusSuggestion;
+impl SuggestionOwner for ThesaurusSuggestion {
+    fn produce(&self) -> Option<InlineQueryResult> {
+        let text = "Or write \"sa.WORD\" to look up synonyms & antonyms";
+        let msg = InputMessageContentText::new(
+            "Write @WordsLookupBot \"sa.WORD\" to look up synonyms & antonyms in the Thesaurus",
+        );
+        let msg = InputMessageContent::Text(msg);
+        let msg = InlineQueryResultArticle::new("syn_ant", text, msg);
+        Some(InlineQueryResult::Article(msg))
+    }
+}
 
-        let syn_ant = {
-            let text = "Or write \"sa.WORD\" to look up synonyms & antonyms";
-            let msg = InputMessageContentText::new(
-                "Write @WordsLookupBot \"sa.WORD\" to look up synonyms & antonyms in the Thesaurus",
-            );
-            let msg = InputMessageContent::Text(msg);
-            let msg = InlineQueryResultArticle::new("syn_ant", text, msg);
-            InlineQueryResult::Article(msg)
-        };
-        answers.push(syn_ant);
-
-        let answer = cache.require_fresh_answer().await?;
+struct WordleSuggestion {
+    wordle: Option<WordleDayAnswer>,
+}
+impl WordleSuggestion {
+    fn compose_message(answer: WordleDayAnswer) -> Option<String> {
         let WordleAnswer {
             solution,
             editor,
@@ -75,17 +74,53 @@ impl SuggestionsOwner {
             days_since_launch, solution, editor
         );
         formatter.append_title(wordle_title);
-        let msg = formatter.compose_word_defs(&solution, &answer.definitions);
-        if let Ok(wordle_message) = msg {
-            let title = "Send definition of today's wordle answer!";
-            let msg =
-                InputMessageContentText::new(wordle_message).parse_mode(ParseMode::MarkdownV2);
-            let msg = InputMessageContent::Text(msg);
-            let article = InlineQueryResultArticle::new("wordle", title, msg);
-            let answer = InlineQueryResult::Article(article);
-            answers.push(answer);
-        }
+        formatter
+            .compose_word_defs(&solution, &answer.definitions)
+            .ok()
+    }
 
+    fn compose_response(message: String) -> InlineQueryResult {
+        let title = "Send definition of today's wordle answer!";
+        let msg = InputMessageContentText::new(message).parse_mode(ParseMode::MarkdownV2);
+        let msg = InputMessageContent::Text(msg);
+        let article = InlineQueryResultArticle::new("wordle", title, msg);
+        InlineQueryResult::Article(article)
+    }
+}
+
+impl SuggestionOwner for WordleSuggestion {
+    fn produce(&self) -> Option<InlineQueryResult> {
+        self.wordle
+            .clone()
+            .and_then(Self::compose_message)
+            .map(Self::compose_response)
+    }
+}
+
+pub struct SuggestionsOwner;
+impl SuggestionsOwner {
+    async fn ensure_wordle_answer(mut cache: WordleCache) -> Option<WordleDayAnswer> {
+        cache
+            .require_fresh_answer()
+            .await
+            .inspect_err(|err| {
+                log::error!("Failed to get today's wordle, err: {}", err);
+            })
+            .ok()
+    }
+
+    async fn suggestions_handler(
+        bot: Bot,
+        query: InlineQuery,
+        wordle: Option<WordleDayAnswer>,
+    ) -> anyhow::Result<()> {
+        let suggestions = vec![
+            HelpSuggestion {}.produce(),
+            UrbanSuggestion {}.produce(),
+            ThesaurusSuggestion {}.produce(),
+            WordleSuggestion { wordle }.produce(),
+        ];
+        let answers = suggestions.into_iter().flatten().collect::<Vec<_>>();
         bot.answer_inline_query(query.id, answers).await?;
         Ok(())
     }
